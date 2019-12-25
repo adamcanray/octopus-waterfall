0299fa539c9b6ec30a0be89f9cc6a03d748f1559
AdamCanray <adamcool180@gmail.com>
2019-12-25 12:38:03 +0700

    menambahkan folder hosting firebase dist/

---
+++ dist/precache-manifest.a585084bba1a54f6f69c51f9743332ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54de229d8ee2a5a4ae65e43cf7a55c75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9c9c05d46c4b43aaab1",
+    "url": "/static/css/main.1ef61e3a.chunk.css"
+  },
+  {
+    "revision": "bf5ca1173254fba0eaee",
+    "url": "/static/js/2.e4063d69.chunk.js"
+  },
+  {
+    "revision": "525313bed2e267680fbec8691eee6ca6",
+    "url": "/static/js/2.e4063d69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9c9c05d46c4b43aaab1",
+    "url": "/static/js/main.a0830a06.chunk.js"
+  },
+  {
+    "revision": "d76f01e264eed2ab8899",
+    "url": "/static/js/runtime-main.ccabc19f.js"
+  }
+]);
